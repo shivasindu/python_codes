8353b899208b9b99661e27c09d787673ee469966
shivasindu <148967968+shivasindu@users.noreply.github.com>
2025-06-02 14:15:11 +0530

    to count vowels and consonants in a string

---
+++ count_vowels_consonants.py
@@ -0,0 +1,12 @@
+a = "hello"
+vowels = "aeiou"
+vowel_count = 0
+consonant_count = 0
+for i in a:
+    if i.isalpha():
+        if i in vowels:
+            vowel_count += 1
+        else:
+            consonant_count += 1
+print("vowels:", vowel_count, "consonants:", consonant_count)
+S
