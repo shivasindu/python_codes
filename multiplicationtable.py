a96cae818fe62dda3de16dc5f6039c17471c9e66
shivasindu <148967968+shivasindu@users.noreply.github.com>
2025-06-11 11:37:23 +0530

    printing multiplication tables from 1 to 10

---
+++ multiplicationtable.py
@@ -0,0 +1,5 @@
+for i in range(1, 11):  # Tables from 1 to 10
+    print("Multiplication Table for", i)
+    for j in range(1, 11):  # Each table from 1 to 10
+        print(i, "x", j, "=", i * j)
+    print()  # Empty line between tables
